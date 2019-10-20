93f3bff3b4b68e7d7c449e7e73c19144fe5000ec
grdvnl <grdvnl@gmail.com>
2019-10-19 21:54:56 -0700

    initial prototype, work in progress

---
+++ delinter/unused_imports.py
@@ -0,0 +1,154 @@
+import os
+import re
+import typing as tp
+import collections
+import dataclasses
+from typing import Set
+from typing import Dict
+from typing import Union
+from collections import defaultdict
+
+import libcst as cst
+
+unused_imports: Dict[Union[cst.Import, cst.ImportFrom], Set[str]] = defaultdict(set)
+undefined_references: Dict[cst.CSTNode, Set[str]] = defaultdict(set)
+
+pylint_str = str # output formatted string of Pylint output
+
+class RemoveUnusedImportTransformer(cst.CSTTransformer):
+    METADATA_DEPENDENCIES = (cst.metadata.SyntacticPositionProvider,)
+
+    @classmethod
+    def build_dotted_name(cls, import_alias: cst.ImportAlias):
+        def walk(node):
+            if isinstance(node, cst.Name):
+                return (node.value,)
+            children = walk(node.value)
+            return children + (node.attr.value,)
+        return walk(import_alias.name)
+
+    def leave_import_alike(
+        self,
+        original_node: tp.Union[cst.Import, cst.ImportFrom],
+        updated_node: tp.Union[cst.Import, cst.ImportFrom],
+    ) -> tp.Union[cst.Import, cst.ImportFrom, cst.RemovalSentinel]:
+        #import ipdb; ipdb.set_trace()
+        pass
+
+    def leave_Import(
+        self, original_node: cst.Import, updated_node: cst.Import
+    ) -> cst.Import:
+        code = self.get_metadata(cst.metadata.SyntacticPositionProvider, original_node)
+
+        new_import_alias = []
+        for import_alias in updated_node.names:
+            dotted_name = self.build_dotted_name(import_alias)
+            if import_alias.asname:
+                continue
+            new_import_alias.append(import_alias)
+        if new_import_alias:
+            new_import_alias[-1] = new_import_alias[-1].with_changes(
+                    comma=cst.MaybeSentinel.DEFAULT)
+            return updated_node.with_changes(names=new_import_alias)
+        if code.start.line == 1:
+            return cst.RemoveFromParent()
+        return updated_node
+
+
+    def leave_ImportFrom(
+        self, original_node: cst.ImportFrom, updated_node: cst.ImportFrom
+    ) -> cst.ImportFrom:
+        #import ipdb; ipdb.set_trace()
+        #import ipdb; ipdb.set_trace()
+        return updated_node
+
+
+class Delinter:
+    pass
+
+@dataclasses.dataclass
+class BaseUnusedImportsWarning:
+    '''
+    Sum type class to represent types
+    '''
+
+@dataclasses.dataclass
+class UnusedFromImportsWarning(BaseUnusedImportsWarning):
+    import_as_name: str
+    dotted_as_name: str
+    alias: str
+
+@dataclasses.dataclass
+class UnusedImportsWarning(BaseUnusedImportsWarning):
+    alias: str
+    dotted_as_name: str
+
+
+class UnusedImportsDelinter(Delinter):
+
+    # filter import without alias
+    pattern_import = re.compile('Unused import (?P<dname>.*)')
+    # filter import with alias
+    pattern_import_with_alias = re.compile('Unused (?P<dname>.*) imported as (?P<aname>.*)')
+
+    # filter from with alias
+    pattern_from_with_alias = re.compile(
+            'Unused (?P<dname>.*) imported from (?P<dname0>.*) as (?P<aname>.*)')
+
+    # filter from
+    pattern_from = re.compile('Unused (?P<dname>.*) imported from (?P<aname>.*)')
+
+    patterns = [
+            (pattern_import, UnusedImportsWarning),
+            (pattern_import_with_alias, UnusedImportsWarning),
+            # the order of from filters is implicit here, to keep the regex simple.
+            (pattern_from_with_alias, UnusedFromImportsWarning),
+            (pattern_from, UnusedFromImportsWarning)]
+
+    @classmethod
+    def parse_linter_warnings(cls,
+            warnings: tp.Iterable[pylint_str]) -> BaseUnusedImportsWarning:
+        '''
+        Filter just the linter warnings
+        '''
+        def parse_warning(warning):
+            for pattern, class_ in cls.patterns:
+                m = re.match(pattern, warning)
+                if m:
+                    groups = m.groups()
+                    if class_ is UnusedImportsWarning:
+                        if len(groups) == 2:
+                            return UnusedImportsWarning(
+                                    alias=m.group('aname'),
+                                    dotted_as_name=m.group('dname')
+                                    )
+                        return UnusedImportsWarning(
+                                alias=None,
+                                dotted_as_name=m.group('dname')
+                                )
+
+                    if class_ is UnusedFromImportsWarning:
+                        if len(groups) == 2:
+                            return UnusedFromImportsWarning(
+                                    import_as_name=m.group('aname'),
+                                    dotted_as_name=m.group('dname'),
+                                    alias=None)
+                        return UnusedFromImportsWarning(
+                                    import_as_name=m.group('dname'),
+                                    dotted_as_name=m.group('dname0'),
+                                    alias=m.group('aname'))
+            raise ValueError(f"Parsing failed for {warning}")
+
+
+        import_warnings = []
+        for warning in warnings:
+            import_warnings.append(parse_warning(warning))
+        return import_warnings
+
+
+    @classmethod
+    def update(cls, cst, warnings):
+        '''
+        Apply the refactor on each of the linter warnings
+        '''
+        pass
---
+++ test/input/test_unused_imports.py
@@ -0,0 +1,21 @@
+#import unitest.mock.patch, unittest.mock.patch as p1
+#import unittest as t
+import unitest.mock.patch as p
+# import os
+# import pandas as pd, numpy as np
+from collections import defaultdict, OrderedDict
+from itertools import filterfalse as _filterfalse
+# import numpy
+# import unittest.mock
+# from unittest.mock import patch as p1, patch as p2
+# # from ... import *
+# # from ....... import *
+# # from ........... import *
+# # from ............... import *
+#import numpy as np1
+#import numpy as np1
+
+# x = OrderedDict()
+
+# def f(x,y):
+#     pass
